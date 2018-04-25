ede6c8227680a8961904ffd52c8b7b7bddf86c36
Ian Stewart <ianmstew@gmail.com>
2018-04-25 19:29:16 -0400

    Epic setup

--- src/Store/middleware/index.js
+++ src/Store/middleware/index.js
@@ -1,5 +1,11 @@
+import epicMiddleware from './epicMiddleware';
 import thunkMiddleware from 'redux-thunk';
 import promiseMiddleware from './promiseMiddleware';
 import loggingMiddleware from './loggingMiddleware';
 
-export default [thunkMiddleware, promiseMiddleware, loggingMiddleware];
+export default [
+  epicMiddleware,
+  thunkMiddleware,
+  promiseMiddleware,
+  loggingMiddleware
+];
---
+++ src/state/epic.js
@@ -0,0 +1,4 @@
+import { combineEpics } from 'redux-observable';
+import * as searchEpics from './search/epics';
+
+export default combineEpics(...Object.values(searchEpics));
